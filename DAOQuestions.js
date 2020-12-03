8b42b08f7d50be4c9a9cc81752f3e7d851de33ab
victor <vicvelaz@ucm.es>
2020-12-03 15:37:50 +0100

    mas métodos de DAOQuestions

--- DAOQuestions.js
+++ DAOQuestions.js
@@ -8,31 +8,63 @@ class DAOQuestions {
   
     isQuestionCorrect(email, password, callback) {
   
-      this.pool.getConnection(function(err, connection) {
-          if (err) { 
-              callback(new Error("Error de conexión a la base de datos"));
-          }
-          else {
-          connection.query("SELECT * FROM user WHERE email = ? AND password = ?" ,
-          [email,password],
-          function(err, rows) {
-              connection.release(); // devolver al pool la conexión
-              if (err) {
-                  callback(new Error("Error de acceso a la base de datos"));
-              }
-              else {
-                  if (rows.length === 0) {
-                      callback(null, false); //no está el usuario con el password proporcionado
-                  }
-                  else {
-                      callback(null, true);
-                  }           
-              }
-          });
-          }
-      }
-      );
+    //   this.pool.getConnection(function(err, connection) {
+    //       if (err) { 
+    //           callback(new Error("Error de conexión a la base de datos"));
+    //       }
+    //       else {
+    //       connection.query("SELECT * FROM user WHERE email = ? AND password = ?" ,
+    //       [email,password],
+    //       function(err, rows) {
+    //           connection.release(); // devolver al pool la conexión
+    //           if (err) {
+    //               callback(new Error("Error de acceso a la base de datos"));
+    //           }
+    //           else {
+    //               if (rows.length === 0) {
+    //                   callback(null, false); //no está el usuario con el password proporcionado
+    //               }
+    //               else {
+    //                   callback(null, true);
+    //               }           
+    //           }
+    //       });
+    //       }
+    //   }
+    //   );
   }  
+
+  getQuestionInfo(){
+
+  }
+
+  setQuestion(){
+
+  }
+
+  getAllQuestions(){
+
+  }
+
+  getQuestionsByTag(){
+
+  }
+
+  getQuestionsByText(){
+      
+  }
+
+  getAnswersByQuestion(){
+
+  }
+
+  setAnswer(){
+
+  }
+
+  
+
+
   
 
   }
